094bd572dc586c1184fa5bedd934c781caaa4c28
ManuelCM21 <manuelchuncamamani@gmail.com>
2021-10-05 12:48:30 -0500

    Sistema Venta

---
+++ src/sistemaventas/SistemaVentas.java
@@ -0,0 +1,13 @@
+
+package sistemaventas;
+
+import Vista.Login;
+
+public class SistemaVentas {
+
+    public static void main(String[] args) {
+        Login lg = new Login();
+        lg.setVisible(true);
+    }
+    
+}
